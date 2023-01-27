de6ded87d0df7ba14da74f830d733850ca911142
poludnev <poludnev.roman@gmail.com>
2023-01-27 14:23:33 +0300

    updated stucture

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,28 +1,104 @@
+import { rejects } from "assert";
 import axios from "axios";
+import { exec } from "child_process";
+import { resolve } from "path";
 
 export function getBlobHeaders(url: string): Promise<any> {
   return axios({
     url,
-    method: 'GET',
-    responseType: 'arraybuffer', // important
+    method: "GET",
+    responseType: "arraybuffer",
   }).then((response) => {
     return {
       blob: response.data,
-      headers: response.headers
-    }
+      headers: response.headers,
+    };
   });
-
-};
+}
 
 export function objectToAttributes(object: any) {
   let attributes = [];
-  for(const key of Object.keys(object)) {
+  for (const key of Object.keys(object)) {
     attributes.push({
-      trait_type:key,
-      value:object[key]
-    })
+      trait_type: key,
+      value: object[key],
+    });
   }
   return attributes;
 }
 
-export default { objectToAttributes, getBlobHeaders };
+export const getDomainInformation = (
+  url: string,
+  timeoutMs: number = 30000
+): Promise<any[]> => {
+  const domainInformation: string[] = [];
+  const errors: any[] = [];
+  exec(`dig ${url} any +trace`, (err, stdout, stderr) => {
+    console.log("std", !stderr, !err);
+    if (!!err) {
+      errors.push(err);
+      return;
+    }
+    if (!!stderr) {
+      errors.push(stderr);
+      return;
+    }
+    if (!err && !stderr) {
+      const res = stdout
+        .split("\n")
+        .filter((el) => el !== "")
+        .map((el) =>
+          el
+            .split("\t")
+            .filter((el) => el !== "")
+            .join(" ")
+        );
+      domainInformation.push(...res);
+    }
+  });
+  return new Promise((resolve, rejects) => {
+    const checkTimeoutMs = 500;
+    const check = (): NodeJS.Timeout | void => {
+      if (errors.length > 0) rejects(errors.join());
+      return domainInformation.length > 0
+        ? resolve(domainInformation)
+        : setTimeout(check, checkTimeoutMs);
+    };
+    check();
+
+    setTimeout(() => {
+      rejects("can not get DNS info");
+    }, timeoutMs);
+  });
+};
+
+export const getIncludesSubstringElementIndex = (
+  array: string[],
+  substring: string,
+  start: number = 0
+): number | null =>
+  array.reduce<number | null>(
+    (acc, val, index) =>
+      val.includes(substring) && acc === null && index >= start ? index : acc,
+    null
+  );
+
+export const getClearHost = (url: string) => {
+  const host = new URL(url).host;
+  return host.slice(host.includes("www") ? 4 : 0);
+};
+
+export const trimUrl = (url: string): string => {
+  const trimmedUrl = url.trim();
+  return trimmedUrl[trimmedUrl.length - 1] === "/"
+    ? trimmedUrl.substring(0, trimmedUrl.length - 1)
+    : trimmedUrl;
+};
+
+export default {
+  objectToAttributes,
+  getBlobHeaders,
+  getDomainInformation,
+  getClearHost,
+  trimUrl,
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
-import { Router } from 'express';
-import controllers from '../controllers';
+import { Router } from "express";
+import controllers from "../controllers";
 
 const router = Router();
 
-router.route('/').get(controllers.getIndexPage);
-router.route('/stamped').get(controllers.stampImage);
-router.route('/meta').get(controllers.getMeta);
-router.route('/proxy').get(controllers.getProxy);
-router.route('/send').post(controllers.makeScreenShot);
-router.route('/adapter_response.json').get(controllers.getAdapterResponseJSON);
+router.route("/").get(controllers.getIndexPage);
+router.route("/stamped").get(controllers.getStampedImage);
+router.route("/meta").get(controllers.getMeta);
+router.route("/proxy").get(controllers.getProxy);
+router.route("/send").post(controllers.getScreenShot);
+router.route("/adapter_response.json").get(controllers.getAdapterResponseJSON);
 
 export default router;
