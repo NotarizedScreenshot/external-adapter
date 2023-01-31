b05a063210ef83bddf5684616451613cd99319ba
poludnev <poludnev.roman@gmail.com>
2023-01-31 18:47:45 +0300

    Add uploading stamped scrshot to nft storage

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,14 +1,21 @@
 import fs from "fs/promises";
+import fss from "fs";
 import { Request, Response } from "express";
 import images from "images";
 import puppeteer, { HTTPResponse } from "puppeteer";
 import { createCanvas } from "canvas";
+import { NFTStorage, Blob as NFTBlob } from 'nft.storage';
+import enchex from 'crypto-js/enc-hex';
+import sha256 from 'crypto-js/sha256';
+import CryptoJS from 'crypto-js';
+
 import {
   getHostWithoutWWW,
   getDomainInformation,
   getIncludeSubstringElementIndex,
   trimUrl,
 } from "../helpers";
+import path from 'path';
 
 const VIEWPORT_DEFAULT_WIDTH = 1000;
 const VIEWPORT_DEFAULT_HEIGHT = 1000;
@@ -68,9 +75,14 @@ const getScreenShot = async (request: Request, response: Response) => {
 
     await page.goto(url, { waitUntil: "networkidle0" });
 
-    const path = `${url.split("/").join("-")}.png`;
+    if (!fss.existsSync(path.resolve(process.env.PWD!, 'data'))) {
+      await fs.mkdir(path.resolve(process.env.PWD!, 'data'));
+    }
 
-    const file = await page.screenshot({ path });
+    const screenshotPath = `${path.resolve(process.env.PWD!, 'data', url.split("/").join("-"))}.png`;
+    console.log(screenshotPath);
+
+    const file = await page.screenshot({ path: screenshotPath });
     response.set("Content-Type", "image/png");
     return response.status(200).send(file);
   } catch (error) {
@@ -78,10 +90,10 @@ const getScreenShot = async (request: Request, response: Response) => {
   }
 };
 
-const getStampedImage = (request: Request, response: Response) => {
+const getStampedImage = async (request: Request, response: Response) => {
 
-  const { file } = request.query as { file: string };
-  const fileName = trimUrl(file).split("/").join("-") + ".png";
+  const { path: filePath } = request.query as { path: string };
+  const fileName = path.resolve(process.env.PWD!, 'data', trimUrl(filePath).split("/").join("-") + ".png");
 
   try {
     const canvas = createCanvas(800, 1000);
@@ -117,6 +129,62 @@ const getStampedImage = (request: Request, response: Response) => {
     const metamarkedImage = watermarkedScreenshotImage.draw(metaImage, 0, 0);
     const metamarkedImageBuffer = metamarkedImage.encode("png");
 
+    await fs.writeFile(path.resolve(process.env.PWD!, 'data', trimUrl(filePath).split("/").join("-") + "-stamped.png"), metamarkedImageBuffer);
+
+    const markedImageBlob = new NFTBlob([metamarkedImageBuffer]);
+
+    // @ts-ignore
+    const trustedSha256sum = enchex.stringify(sha256( CryptoJS.lib.WordArray.create(metamarkedImageBuffer)));
+
+    const client = new NFTStorage({ token: process.env.NFT_STORAGE_TOKEN! });
+
+    const markedImageContentId = await client.storeBlob(markedImageBlob);
+
+    console.log('markedImageContentId', markedImageContentId)
+
+
+
+    const name = "Notarized Screenshot 0x" + trustedSha256sum;
+    const image = "ipfs://" + markedImageContentId;
+    // const image = "ipfs://" + 'markedImageContentId';;
+    const ts = Date.now()
+    const time = new Date(ts).toUTCString();
+    const url = filePath;
+    const description = name +
+        " by QuantumOracle, result of verifying the image served at URL \n" +
+        url +
+        " at ts " + time + "\n" +
+        " Check metadata fields for more details."
+
+    const metadata = {
+        name,
+        image,
+        description,
+        ts,
+        time,
+        url,
+        meta: metaString
+        
+    }
+
+    // console.log(metadata);
+
+    const metadataBlob = new NFTBlob([JSON.stringify(metadata)]);
+
+    const metadataBlobContentId = await client.storeBlob(metadataBlob);
+
+    console.log('metadataBlobContentId', metadataBlobContentId);
+
+    const storedData = { 
+      imageLink: `https://ipfs.io/ipfs/${markedImageContentId}`,
+      jsonLink: `https://ipfs.io/ipfs/${metadataBlobContentId}`,
+      openseaLink: `https://opensea.io/assets/matic/0xa567349bdd3d4f2c3e25f65745a020162c202ef2/${BigInt(
+                '0x' + trustedSha256sum,
+              ).toString(10)}`,  
+    }
+
+    await fs.writeFile(path.resolve(process.env.PWD!, 'data', trimUrl(filePath).split("/").join("-") + "-data.json"), JSON.stringify(storedData), "utf-8");
+
     response.set("Content-Type", "image/png");
     return response.status(200).send(metamarkedImageBuffer);
   } catch (error) {
@@ -125,8 +193,22 @@ const getStampedImage = (request: Request, response: Response) => {
   }
 };
 
+const getStoredData = async (request: Request, response: Response) => {
+  try {
+    const { path: filePath } = request.query as { path: string };
+    const fileName = path.resolve(process.env.PWD!, 'data', trimUrl(filePath).split("/").join("-") + "-data.json");
+    const data = await fs.readFile(fileName, 'utf-8');
+    console.log(data)
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(502).send(error);
+  }
+
+}
+
 export default {
   getIndexPage,
   getStampedImage,
   getScreenShot,
+  getStoredData,
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 router.route("/").get(controllers.getIndexPage);
 router.route("/stamped").get(controllers.getStampedImage);
+router.route("/storedData").get(controllers.getStoredData);
 router.route("/send").post(controllers.getScreenShot);
 
 export default router;
