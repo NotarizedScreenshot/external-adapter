bf8c4e970d24846a0dbc07686b8e1a78a1ec7a01
poludnev <poludnev.roman@gmail.com>
2023-03-31 14:28:54 +0300

    controllers cleanup

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,10 @@
 import { Router } from 'express';
-import controllers, { getIndexPage, getScreenShot, adapterResponse } from '../controllers';
+import { getIndexPage, getScreenShot, adapterResponse } from '../controllers';
 
 const router = Router();
 
 router.route('/').get(getIndexPage);
 router.route('/previewData').get(getScreenShot);
-router.route('/stamped').get(controllers.getStampedImage);
-router.route('/tweetData').get(controllers.getTweetData);
-router.route('/metaData').get(controllers.getMetaData);
-router.route('/send').post(controllers.getScreenShot);
-// router.route('/adapter_response.json').post(controllers.adapterResponseJSON);
 router.route('/adapter_response.json').post(adapterResponse);
 
 export default router;
