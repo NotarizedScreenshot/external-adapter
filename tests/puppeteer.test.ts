0dff01cc633635de5c51b095b7e155daa5755928
elizavetta <elizavetta.net@gmail.com>
2023-04-01 02:25:37 +0300

    puppeteer test case fix

--- tests/puppeteer.test.ts
+++ tests/puppeteer.test.ts
@@ -25,7 +25,7 @@ describe('testing puppeteer', async () => {
   describe('testing Browser is on', () => {
     it('browser already launched', (done) => {
       expect(browser.isConnected()).to.true;
-      expect(browser.process()).to.equal(null);
+      expect(browser.process()).not.to.equal(null);
       
       done();
     });
@@ -43,7 +43,7 @@ describe('testing puppeteer', async () => {
       const page = await browser.newPage();
       expect ((async () => {
         await page.goto('https://frrfgrtgtrgtg.rtgtg/', { waitUntil: 'networkidle0' });
-      })).to.throw(Error)
+      })).to.throw()
       
     });
 
