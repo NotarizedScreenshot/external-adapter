a1984ee5438fda60afe9acf107aaa08774479ccc
poludnev <poludnev.roman@gmail.com>
2023-04-21 22:35:22 +0300

    Fixes after review

--- src/controllers/getScreenShot.ts
+++ src/controllers/getScreenShot.ts
@@ -4,12 +4,7 @@ import { screenshotWithPuppeteer } from '../helpers';
 export const getScreenShot = async (request: Request, response: Response) => {
   try {
     return screenshotWithPuppeteer(request, response);
-  } catch (error) {
-    console.log(`controller error:  ${error}`);
-    if (error instanceof Error) {
-      return response.status(502).json({ error: error.message });
-    }
-    response.status(502).json({ error: `Unknown error: ${error}` });
+  } catch (error: any) {
+    response.status(502).json({ error: error.message });
   }
 };
-// 
