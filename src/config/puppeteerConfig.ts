12a521d829d0b286f1b93a4451b69e33b0e4cc36
poludnev <poludnev.roman@gmail.com>
2023-05-04 21:20:15 +0300

    test error response

--- src/config/puppeteerConfig.ts
+++ src/config/puppeteerConfig.ts
@@ -10,8 +10,8 @@ export const puppeteerDefaultConfig: {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   },
   viewport: {
-    width: 1024,
-    height: 1024,
+    width: 1440,
+    height: 2024,
   },
   userAgent:
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
