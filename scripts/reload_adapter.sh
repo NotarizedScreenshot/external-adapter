325c369c70d3daebbf06dfb866d16b2ded9a67bf
etsvigun <utgarda@gmail.com>
2023-05-06 15:33:39 +0300

    Github Actions: docker-compose restart with new image tag #26

--- scripts/reload_adapter.sh
+++ scripts/reload_adapter.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/sh
+export TAGS=${1:-latest}
+export TAG=$(echo "$TAGS" | cut -d',' -f1)
 docker compose stop notaryshot-adapter
 docker compose rm --stop --force notaryshot-adapter 
 docker compose pull
