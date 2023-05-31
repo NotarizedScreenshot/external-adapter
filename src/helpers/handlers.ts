602a8d04b4af0ee34c77e83d6db881542e36407d
poludnev <poludnev.roman@gmail.com>
2023-05-31 11:22:01 +0200

    #66 revised adapter_response handler

--- src/helpers/handlers.ts
+++ src/helpers/handlers.ts
@@ -7,12 +7,8 @@ import {
   makeTweetUrlWithId,
   trimUrl,
 } from '../helpers';
-import {Browser, HTTPResponse, Page} from 'puppeteer';
-import {
-  IGetScreenshotResponseData,
-  IMetadata,
-  ITweetTimelineEntry,
-} from '../types';
+import { Browser, HTTPResponse, Page } from 'puppeteer';
+import { IGetScreenshotResponseData, IMetadata, ITweetTimelineEntry } from '../types';
 import { DEFAULT_TIMEOUT_MS, screenshotResponseDataOrderedKeys } from '../config';
 import { Request, Response } from 'express';
 import { reportError } from '../helpers';
@@ -84,108 +80,114 @@ const getScreenshotWithPuppeteer = async (
   request: Request,
   response: Response,
 ): Promise<Response> => {
-  const { tweetId, userId } = request.query as {
-    tweetId: string;
-    userId: string;
-  };
-  const jobs = await uploadQueue.getJobs(['active']);
-
-  const activeJob = jobs.find((job) => job.data.userId === userId);
-
-  if (!isValidUint64(tweetId)) {
-    return reportError('invalid tweeetId', response);
-  }
-  if (!!activeJob) {
-    const { stampedImageBuffer, metadata, tweetdata } = activeJob.data;
-
-    const responseData: IGetScreenshotResponseData = {
-      imageUrl: makeImageBase64UrlfromBuffer(Buffer.from(stampedImageBuffer!)),
-      metadata,
-      tweetdata,
+  try {
+    const { tweetId, userId } = request.query as {
+      tweetId: string;
+      userId: string;
     };
-    response.set('Content-Type', 'application/json');
-    return response.status(200).send({ ...responseData, jobId: activeJob.id });
-  }
+    const jobs = await uploadQueue.getJobs(['active']);
 
-  const tweetUrl = makeTweetUrlWithId(tweetId);
+    const activeJob = jobs.find((job) => job.data.userId === userId);
 
-  console.log('tweetUrl: ', tweetUrl);
+    if (!isValidUint64(tweetId)) {
+      return reportError('invalid tweeetId', response);
+    }
+    if (!!activeJob) {
+      const { stampedImageBuffer, metadata, tweetdata } = activeJob.data;
 
-  const browser = await getBrowser();
+      const responseData: IGetScreenshotResponseData = {
+        imageUrl: makeImageBase64UrlfromBuffer(Buffer.from(stampedImageBuffer!)),
+        metadata,
+        tweetdata,
+      };
+      response.set('Content-Type', 'application/json');
+      return response.status(200).send({ ...responseData, jobId: activeJob.id });
+    }
 
-  const page = await browser.newPage();
+    const tweetUrl = makeTweetUrlWithId(tweetId);
 
-  console.log('page', page);
+    console.log('tweetUrl: ', tweetUrl);
 
-  await page.setViewport({
-    ...puppeteerDefaultConfig.viewport,
-    deviceScaleFactor: 1,
-  });
-  await page.setUserAgent(puppeteerDefaultConfig.userAgent);
+    const browser = await getBrowser();
 
-  const screenShotPromise: Promise<string> = page
-    .goto(tweetUrl, puppeteerDefaultConfig.page.goto.gotoWaitUntilIdle)
-    .then(async () => {
-      const articleElement = (await page.waitForSelector('article'))!;
-      const boundingBox = (await articleElement.boundingBox())!;
+    const page = await browser.newPage();
 
-      const screenshotImageBuffer: Buffer = await page.screenshot({
-        clip: { ...boundingBox },
+    console.log('page', page);
+
+    await page.setViewport({
+      ...puppeteerDefaultConfig.viewport,
+      deviceScaleFactor: 1,
+    });
+    await page.setUserAgent(puppeteerDefaultConfig.userAgent);
+
+    const screenShotPromise: Promise<string> = page
+      .goto(tweetUrl, puppeteerDefaultConfig.page.goto.gotoWaitUntilIdle)
+      .then(async () => {
+        const articleElement = (await page.waitForSelector('article'))!;
+        const boundingBox = (await articleElement.boundingBox())!;
+
+        const screenshotImageBuffer: Buffer = await page.screenshot({
+          clip: { ...boundingBox },
+        });
+
+        return makeImageBase64UrlfromBuffer(screenshotImageBuffer);
       });
 
-      return makeImageBase64UrlfromBuffer(screenshotImageBuffer);
+    const promises: Iterable<Promise<string>> = [
+      getTweetDataPromise(page, tweetId),
+      getMetaDataPromise(page, tweetId),
+      screenShotPromise,
+    ];
+    const allData = await Promise.allSettled<string>(promises);
+    const fetchedData = allData.reduce<IGetScreenshotResponseData>(
+      (acc, val, index) => {
+        acc[screenshotResponseDataOrderedKeys[index]] =
+          val.status === 'fulfilled' ? val.value : null;
+        return acc;
+      },
+      {
+        imageUrl: null,
+        metadata: null,
+        tweetdata: null,
+      },
+    );
+
+    const screenshotImageUrl = fetchedData.imageUrl;
+    const screenshotImageBuffer = makeBufferFromBase64ImageUrl(screenshotImageUrl!);
+    const stampedImageBuffer = await makeStampedImage(screenshotImageUrl!);
+    const stampedImageUrl = makeImageBase64UrlfromBuffer(stampedImageBuffer!);
+    const responseData: IGetScreenshotResponseData = { ...fetchedData, imageUrl: stampedImageUrl };
+
+    const tweetEntry: ITweetTimelineEntry = getTweetTimelineEntries(responseData.tweetdata!).find(
+      (entry) => entry.entryId === `tweet-${tweetId}`,
+    )!;
+
+    const tweetData = createTweetData(tweetEntry.content.itemContent.tweet_results.result);
+
+    const tweetsDataUrlsToUpload = tweetData ? getMediaUrlsToUpload(tweetData) : [];
+    //TODO: Issue 52: https://github.com/orgs/NotarizedScreenshot/projects/1/views/1?pane=issue&itemId=27498718\
+    //Add handling tombstone tweet
+
+    const mediaUrls = Array.from(new Set([...tweetsDataUrlsToUpload]));
+
+    const uploadJob = await uploadQueue.add({
+      tweetId,
+      userId,
+      metadata: fetchedData.metadata,
+      tweetdata: fetchedData.tweetdata,
+      screenshotImageBuffer,
+      stampedImageBuffer,
+      mediaUrls,
     });
 
-  const promises: Iterable<Promise<string>> = [
-    getTweetDataPromise(page, tweetId),
-    getMetaDataPromise(page, tweetId),
-    screenShotPromise,
-  ];
-  const allData = await Promise.allSettled<string>(promises);
-  const fetchedData = allData.reduce<IGetScreenshotResponseData>(
-    (acc, val, index) => {
-      acc[screenshotResponseDataOrderedKeys[index]] = val.status === 'fulfilled' ? val.value : null;
-      return acc;
-    },
-    {
-      imageUrl: null,
-      metadata: null,
-      tweetdata: null,
-    },
-  );
+    await browser.close();
 
-  const screenshotImageUrl = fetchedData.imageUrl;
-  const screenshotImageBuffer = makeBufferFromBase64ImageUrl(screenshotImageUrl!);
-  const stampedImageBuffer = await makeStampedImage(screenshotImageUrl!);
-  const stampedImageUrl = makeImageBase64UrlfromBuffer(stampedImageBuffer!);
-  const responseData: IGetScreenshotResponseData = { ...fetchedData, imageUrl: stampedImageUrl };
-
-  const tweetEntry: ITweetTimelineEntry = getTweetTimelineEntries(responseData.tweetdata!).find(
-    (entry) => entry.entryId === `tweet-${tweetId}`,
-  )!;
-
-  const tweetData = createTweetData(tweetEntry.content.itemContent.tweet_results.result);
-
-  const tweetsDataUrlsToUpload = tweetData ? getMediaUrlsToUpload(tweetData) : [];
-  //TODO: Issue 52: https://github.com/orgs/NotarizedScreenshot/projects/1/views/1?pane=issue&itemId=27498718\
-  //Add handling tombstone tweet
-
-  const mediaUrls = Array.from(new Set([...tweetsDataUrlsToUpload]));
-
-  const uploadJob = await uploadQueue.add({
-    tweetId,
-    userId,
-    metadata: fetchedData.metadata,
-    tweetdata: fetchedData.tweetdata,
-    screenshotImageBuffer,
-    stampedImageBuffer,
-    mediaUrls,
-  });
-
-  await browser.close();
-
-  response.set('Content-Type', 'application/json');
-  return response.status(200).send({ ...responseData });
+    response.set('Content-Type', 'application/json');
+    return response.status(200).send({ ...responseData });
+  } catch (error: any) {
+    console.log(error.message);
+    return response.status(500).send({ error: error.message });
+  }
 };
 
 async function getBrowser(): Promise<Browser> {
