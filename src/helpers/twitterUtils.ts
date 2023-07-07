33ae2b87edb8b4594bb557d642b8fee951a05e12
poludnev <poludnev.roman@gmail.com>
2023-07-07 17:08:50 +0200

    #80 add logs

--- src/helpers/twitterUtils.ts
+++ src/helpers/twitterUtils.ts
@@ -2,14 +2,17 @@ import { createTweetData } from '../models';
 import { IThreadData, IThreadEntry, ITweetData, ITweetTimelineEntry } from 'types';
 
 export const getTweetResults = (data: any) => {
-  console.log('getTweetResults', data);
+  console.log('getTweetResults data', data);
+  console.log('getTweetResults data.tweetResult', data.tweetResult);
+  console.log('getTweetResults data', data?.tweetResult);
+
   try {
     switch (true) {
       case !!data.content?.itemContent?.tweet_results?.result:
         console.log('data.contetnt', data.content?.itemContent?.tweet_results?.result);
         return data.content.itemContent.tweet_results.result;
-      case !!data.tweetResult.result:
-        console.log('data.tweetResult', data.tweetResult.result);
+      case !!data.tweetResult?.result:
+        console.log('data.tweetResult', data.tweetResult?.result);
         return data.tweetResult.result;
       default:
         throw new Error(`can not get tweet results, data: ${JSON.stringify(data)}`);
